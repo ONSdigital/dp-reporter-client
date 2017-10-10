8c90494b7b962411cb1765ca80a848193f521d22
Dave Llewellyn <439837@gmail.com>
2017-10-10 09:47:26 +0100

    refactoring

--- reporter/report_event_client_test.go
+++ reporter/report_event_client_test.go
@@ -1,4 +1,4 @@
-package client
+package reporter
 
 import (
 	"errors"
@@ -6,8 +6,8 @@ import (
 
 	"fmt"
 
-	"github.com/ONSdigital/dp-reporter-client/client/clienttest"
 	"github.com/ONSdigital/dp-reporter-client/model"
+	"github.com/ONSdigital/dp-reporter-client/reporter/reportertest"
 	"github.com/ONSdigital/dp-reporter-client/schema"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -27,13 +27,13 @@ var (
 )
 
 func TestHandler_Handle(t *testing.T) {
-	Convey("Given ReporterClient has been configured correctly", t, func() {
+	Convey("Given ImportErrorReporter has been configured correctly", t, func() {
 		marshalParams := make([]interface{}, 0)
 		output, kafkaProducer, marshalFunc := setup(&marshalParams, schema.ReportEventSchema.Marshal)
-		target := &ReporterClient{kafkaProducer: kafkaProducer, serviceName: "Bob", marshal: marshalFunc}
+		target := ImportErrorReporter{kafkaProducer: kafkaProducer, serviceName: "Bob", marshal: marshalFunc}
 
-		Convey("When ReportError is called with valid parameters", func() {
-			err := target.ReportError(testInstanceID, errContext, cause, nil)
+		Convey("When Notify is called with valid parameters", func() {
+			err := target.Notify(testInstanceID, errContext, cause)
 
 			avroBytes := <-output
 
@@ -41,12 +41,12 @@ func TestHandler_Handle(t *testing.T) {
 				So(err, ShouldBeNil)
 			})
 
-			Convey("And Marshall is called once with the expectedReportEvent parameters", func() {
+			Convey("And Marshall is called once with the expected parameters", func() {
 				So(len(marshalParams), ShouldEqual, 1)
 				So(marshalParams[0], ShouldResemble, expectedReportEvent)
 			})
 
-			Convey("And kafkaProducer.Output is called once with the expectedReportEvent parameters", func() {
+			Convey("And kafkaProducer.Output is called once with the expected parameters", func() {
 				var actual model.ReportEvent
 				schema.ReportEventSchema.Unmarshal(avroBytes, &actual)
 
@@ -59,22 +59,22 @@ func TestHandler_Handle(t *testing.T) {
 }
 
 func TestHandler_HandleMarshalError(t *testing.T) {
-	Convey("Given ReporterClient has been configured correctly", t, func() {
+	Convey("Given ImportErrorReporter has been configured correctly", t, func() {
 
 		marshalParams := make([]interface{}, 0)
 		_, kafkaProducer, marshalFunc := setup(&marshalParams, func(s interface{}) ([]byte, error) {
 			return nil, errors.New("Bork!")
 		})
-		target := &ReporterClient{kafkaProducer: kafkaProducer, serviceName: "Bob", marshal: marshalFunc}
+		target := &ImportErrorReporter{kafkaProducer: kafkaProducer, serviceName: "Bob", marshal: marshalFunc}
 
 		Convey("When marshal returns an error", func() {
-			err := target.ReportError(testInstanceID, errContext, cause, nil)
+			err := target.Notify(testInstanceID, errContext, cause)
 
 			Convey("Then the error returned to the caller", func() {
 				So(err, ShouldResemble, errors.New("Bork!"))
 			})
 
-			Convey("And Marshall is called once with the expectedReportEvent parameters", func() {
+			Convey("And Marshall is called once with the expected parameters", func() {
 				So(len(marshalParams), ShouldEqual, 1)
 				So(marshalParams[0], ShouldResemble, expectedReportEvent)
 			})
@@ -87,16 +87,16 @@ func TestHandler_HandleMarshalError(t *testing.T) {
 }
 
 func TestHandler_HandleInstanceIDEmpty(t *testing.T) {
-	Convey("Given ReporterClient has been configured correctly", t, func() {
+	Convey("Given ImportErrorReporter has been configured correctly", t, func() {
 		marshalParams := make([]interface{}, 0)
 		_, kafkaProducer, marshalFunc := setup(&marshalParams, schema.ReportEventSchema.Marshal)
-		target := &ReporterClient{kafkaProducer: kafkaProducer, serviceName: "Bob", marshal: marshalFunc}
+		target := &ImportErrorReporter{kafkaProducer: kafkaProducer, serviceName: "Bob", marshal: marshalFunc}
 
-		Convey("When ReportError is called with an empty instanceID", func() {
-			err := target.ReportError("", errContext, cause, nil)
+		Convey("When Notify is called with an empty instanceID", func() {
+			err := target.Notify("", errContext, cause)
 
 			Convey("Then the expected error is returned", func() {
-				So(err, ShouldResemble, errors.New(instanceEmpty))
+				So(err, ShouldResemble, errors.New(idEmpty))
 			})
 
 			Convey("And marshal is never called", func() {
@@ -111,13 +111,13 @@ func TestHandler_HandleInstanceIDEmpty(t *testing.T) {
 }
 
 func TestHandler_HandleErrContextEmpty(t *testing.T) {
-	Convey("Given ReporterClient has been configured correctly", t, func() {
+	Convey("Given ImportErrorReporter has been configured correctly", t, func() {
 		marshalParams := make([]interface{}, 0)
 		_, kafkaProducer, marshalFunc := setup(&marshalParams, schema.ReportEventSchema.Marshal)
-		target := &ReporterClient{kafkaProducer: kafkaProducer, serviceName: "Bob", marshal: marshalFunc}
+		target := &ImportErrorReporter{kafkaProducer: kafkaProducer, serviceName: "Bob", marshal: marshalFunc}
 
-		Convey("When ReportError is called with an empty errContext", func() {
-			err := target.ReportError(testInstanceID, "", cause, nil)
+		Convey("When Notify is called with an empty errContext", func() {
+			err := target.Notify(testInstanceID, "", cause)
 
 			Convey("Then the expected error is returned", func() {
 				So(err, ShouldResemble, errors.New(contextEmpty))
@@ -139,10 +139,10 @@ func TestNewReporterClient(t *testing.T) {
 		serviceName := "awesome-service"
 
 		Convey("When NewReportClient is called", func() {
-			cli, err := NewReporterClient(nil, serviceName)
+			cli, err := NewImportErrorReporter(nil, serviceName)
 
 			Convey("Then cli is nil and the expected error is returned", func() {
-				So(cli, ShouldBeNil)
+				So(cli, ShouldResemble, ImportErrorReporter{})
 				So(err, ShouldResemble, errors.New(kafkaProducerNil))
 			})
 		})
@@ -150,13 +150,13 @@ func TestNewReporterClient(t *testing.T) {
 
 	Convey("Given serviceName is empty", t, func() {
 		serviceName := ""
-		kafkaProducer := &clienttest.KafkaProducerMock{}
+		kafkaProducer := &reportertest.KafkaProducerMock{}
 
 		Convey("When NewReportClient is called", func() {
-			cli, err := NewReporterClient(kafkaProducer, serviceName)
+			cli, err := NewImportErrorReporter(kafkaProducer, serviceName)
 
 			Convey("Then cli is nil and the expected error is returned", func() {
-				So(cli, ShouldBeNil)
+				So(cli, ShouldResemble, ImportErrorReporter{})
 				So(err, ShouldResemble, errors.New(serviceNameEmpty))
 			})
 		})
@@ -164,10 +164,10 @@ func TestNewReporterClient(t *testing.T) {
 
 	Convey("Given a valid kafkaProducer and serviceName", t, func() {
 		serviceName := "awesome-service"
-		kafkaProducer := &clienttest.KafkaProducerMock{}
+		kafkaProducer := &reportertest.KafkaProducerMock{}
 
 		Convey("When NewReportClient is called", func() {
-			cli, err := NewReporterClient(kafkaProducer, serviceName)
+			cli, err := NewImportErrorReporter(kafkaProducer, serviceName)
 
 			Convey("Then cli is configured as expected", func() {
 				So(cli.serviceName, ShouldEqual, serviceName)
@@ -183,9 +183,9 @@ func TestNewReporterClient(t *testing.T) {
 	})
 }
 
-func setup(marshalParams *[]interface{}, marshal func(s interface{}) ([]byte, error)) (chan []byte, *clienttest.KafkaProducerMock, marshalFunc) {
+func setup(marshalParams *[]interface{}, marshal func(s interface{}) ([]byte, error)) (chan []byte, *reportertest.KafkaProducerMock, marshalFunc) {
 	output := make(chan []byte, 1)
-	producer := clienttest.NewKafkaProducerMock(output)
+	producer := reportertest.NewKafkaProducerMock(output)
 
 	marshalFunc := func(s interface{}) ([]byte, error) {
 		*marshalParams = append(*marshalParams, s)
