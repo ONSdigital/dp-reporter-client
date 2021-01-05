a9371e352bc6d2aa80e8632e52da7d0f8d1e9a8e
outsidecoder <neill.hunter@gmail.com>
2021-01-05 18:30:48 +0000

    Adding CI folder for dp-reporter-client to test new Library pipeline.

---
+++ ci/scripts/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+
+# cwd=$(pwd)
+
+# pushd $cwd/dp-dimension-search-api
+make build ##&& mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
+  # cp Dockerfile.concourse $cwd/build
+# popd
