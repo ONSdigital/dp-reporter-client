fd7a88320e18a268beb3f76ecbec481d6faed003
outsidecoder <neill.hunter@gmail.com>
2021-02-10 15:23:52 +0000

    Changed to make build and make audit.

--- ci/scripts/audit.sh
+++ ci/scripts/audit.sh
@@ -3,5 +3,5 @@
 cwd=$(pwd)
 
 pushd $cwd/dp-reporter-client
-  nancy build && mv build/dp-reporter-client $cwd/build
+  make audit
 popd
--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,6 +3,5 @@
 cwd=$(pwd)
 
 pushd $cwd/dp-reporter-client
-  make build && mv build/dp-reporter-client $cwd/build
-  cp Dockerfile.concourse $cwd/build
+  make build
 popd
